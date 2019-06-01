df041169040b1c2de6dce0063e9274a638d05a8b
stevekutz <stkutz@gmail.com>
2019-06-01 12:01:59 -0500

    GET /projects completed

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -4,7 +4,17 @@ const Projects = require('./projectModel.js');
 
 const router = express.Router();
 
-
+// GET all projects
+router.get('/', async (req, res) =>{
+    try{
+        const projects = await Projects.get();
+        res.status(200).json(projects);
+    } catch (err) {
+        res.status(500).json({
+            message: `Error tryingto access Projects`
+        });
+    }
+});
 
 
 

d21d19ce35b72e0659f29d22f290a8be05ffafe3
stevekutz <stkutz@gmail.com>
2019-06-01 12:55:54 -0500

    GET /actions completed

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -4,8 +4,17 @@ const Actions = require('./actionModel.js');
 
 const router = express.Router();
 
-
-
+// GET all actions
+router.get('/', async (req, res) =>{
+    try{
+        const actions = await Actions.get();
+        res.status(200).json(actions);
+    } catch (err) {
+        res.status(500).json({
+            message: `Error trying to access Actions`
+        });
+    }
+});
 
 
 
